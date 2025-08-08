69bb7efe49805016938151a2ccc0d45cb83de4cb
Gabychytaa <gaby.gmiranda95@gmail.com>
2025-08-08 15:12:30 +0200

    conflict fixed

--- new_math.py
+++ new_math.py
@@ -1,7 +1,14 @@
 def add(x,y):
-    return x+y +10
+    return x+y
 
 def substract (x,y):
     return x-y
+
 def division(x,y):
     return x/y
+
+def square(x):
+    return x**2
+
+def something():
+    return "nice"
